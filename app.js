869ee54918921626913c8e26b6a3107f4ba67523
Alvin Mantovani <alvin.ictn@gmail.com>
2025-04-16 04:58:35 +0700

    init: project

---
+++ app.js
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import moment from "moment";
+import fetch from "node-fetch";
+import fs from "fs";
+import { readJsonFile } from "./libs/read-file.js";
+
+let current = moment(new Date());
+
+const WAKATIME_API_KEY = process.env.WAKATIME_API_KEY;
+const WAKATIME_ID = process.env.WAKATIME_ID;
+
+let data = {};
+async function fetchWakaTime(date) {
+  const response = await fetch(
+    `https://wakatime.com/api/v1/users/${WAKATIME_ID}/summaries?start=${date}&end=${date}`,
+    {
+      headers: {
+        Authorization: `Basic ${WAKATIME_API_KEY}`,
+      },
+    }
+  );
+  const data = await response.json();
+  const dataFetch = data.data[0]  
+  console.log("DATA", data.data[0]);
+}
+
+let dadate = current.format("YYYY-MM-DD");
+// fetchWakaTime(dadate);
+// console.log("current", current.format("YYYY-MM-DD"));
+
+let gg = await readJsonFile("./wakatime-alvin.ictngmail.com-a9b7492e11d446a89da8eb4c7708f9f3.json")
+console.log("da", gg)
